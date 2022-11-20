bfb789c192a68ad3c64a049a57f1ed23e881bab5
Fantacy-Fan <artem.harov.120@mail.ru>
2022-11-20 22:10:59 +0300

    add Pin model

--- MiracleArt/models.py
+++ MiracleArt/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:  # Абстрактная модель
+        abstract = True
+
+
+class Pin(BaseModel):
+    title = models.CharField(max_length=30, null=False, blank=False)
+    text = models.TextField(max_length=90, null=True, blank=True)
+    img = models.ImageField(upload_to='images/', null=False, blank=False)
+    slug = models.SlugField(default='', db_index=True)
+
+    def __str__(self):
+        return f'Pin {self.id} - {self.title}'
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Pin, self).save(*args, **kwargs)
+
+    def get_url(self):
+        return reverse('pin_detail', args=[self.id])
