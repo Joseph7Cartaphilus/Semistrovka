4aae196a750427c3483d108f9c522f7218fafa78
Fantacy-Fan <artem.harov.120@mail.ru>
2022-11-21 17:51:51 +0300

    edit mistakes

--- MiracleArt/models.py
+++ MiracleArt/models.py
@@ -9,20 +9,6 @@ class BaseModel(models.Model):
         abstract = True
 
 
-class Pin(BaseModel):
-    title = models.CharField(max_length=30, null=False, blank=False)
-    text = models.TextField(max_length=90, null=True, blank=True)
-    img = models.ImageField(upload_to='images/', null=False, blank=False)
-    slug = models.SlugField(default='', db_index=True)
-    category = models.ForeignKey(PinCategory, on_delete=models.PROTECT)
-
-    def __str__(self):
-        return f'Pin {self.id} - {self.title}'
-
-    def get_url(self):
-        return reverse('pin_detail', args=[self.id])
-
-
 class PinCategory(models.Model):
     name = models.CharField(max_length=64, unique=True)
     description = models.TextField(null=True, blank=True)
@@ -32,3 +18,17 @@ class PinCategory(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Pin(BaseModel):
+    title = models.CharField(max_length=30, null=False, blank=False)
+    text = models.TextField(max_length=90, null=True, blank=True)
+    img = models.ImageField(upload_to='images/', null=False, blank=False)
+    slug = models.SlugField(default='', db_index=True)
+    category = models.ForeignKey(PinCategory, on_delete=models.PROTECT, blank=True, null=True)
+
+    def __str__(self):
+        return f'Pin {self.id} - {self.title}'
+
+    def get_url(self):
+        return reverse('pin_detail', args=[self.id])
