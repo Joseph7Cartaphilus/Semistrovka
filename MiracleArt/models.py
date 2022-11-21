18bb3872ea15b4f4965aa9ec763929e7571fdac8
Fantacy-Fan <artem.harov.120@mail.ru>
2022-11-21 17:41:06 +0300

    add PinCategory model

--- MiracleArt/models.py
+++ MiracleArt/models.py
@@ -14,9 +14,21 @@ class Pin(BaseModel):
     text = models.TextField(max_length=90, null=True, blank=True)
     img = models.ImageField(upload_to='images/', null=False, blank=False)
     slug = models.SlugField(default='', db_index=True)
+    category = models.ForeignKey(PinCategory, on_delete=models.PROTECT)
 
     def __str__(self):
         return f'Pin {self.id} - {self.title}'
 
     def get_url(self):
         return reverse('pin_detail', args=[self.id])
+
+
+class PinCategory(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    description = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = 'Pin Categories'
+
+    def __str__(self):
+        return self.name
