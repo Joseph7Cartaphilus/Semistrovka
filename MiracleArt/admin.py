6439c753b1a14634518c0336ffffdc7065a91f02
Fantacy-Fan <artem.harov.120@mail.ru>
2022-11-21 17:52:14 +0300

    add model category in admin panel

--- MiracleArt/admin.py
+++ MiracleArt/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
 
-from .models import Pin
+from .models import Pin, PinCategory
 
 
 @admin.register(Pin)
 class PinAdmin(admin.ModelAdmin):
-    fields = ['title', 'text', 'img', 'slug']
+    fields = ['title', 'text', 'category', 'img', 'slug']
     prepopulated_fields = {'slug': ('title',)}
-    list_display = ['title', 'text', 'img', 'slug']
+    list_display = ['title', 'text', 'category', 'img', 'slug']
     ordering = ['title']
     list_per_page = 10
+
+
+@admin.register(PinCategory)
+class PinCategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+    list_per_page = 5
+    ordering = ['name']
