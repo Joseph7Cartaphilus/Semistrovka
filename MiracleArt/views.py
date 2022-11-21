ea8a2a584243723c871f3daff35b894f3b59b87f
Fantacy-Fan <artem.harov.120@mail.ru>
2022-11-21 17:38:19 +0300

    add gallery view

--- MiracleArt/views.py
+++ MiracleArt/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
+from .models import Pin
+
 
 def index(request):
     return render(request, 'index.html')
+
+
+def gallery(request):
+    context = {
+        'pins': Pin.objects.all(),
+    }
+    return render(request, 'gallery.html', context)
