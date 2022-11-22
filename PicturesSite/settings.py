a72351dd754e4b5514485de77002c300c3b92adf
Fantacy-Fan <artem.harov.120@mail.ru>
2022-11-22 17:27:30 +0300

    changed db sqlite to postgresql

--- PicturesSite/settings.py
+++ PicturesSite/settings.py
@@ -75,11 +75,16 @@ WSGI_APPLICATION = 'PicturesSite.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "MiracleArt",
+        "USER": "postgres",
+        "PASSWORD": "17456",
+        "HOST": "localhost",
+        "PORT": 5432
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
