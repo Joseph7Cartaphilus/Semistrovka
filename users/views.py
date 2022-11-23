9f85278a60bfa96ac97ea21d1069ecb79a65adb0
Fantacy-Fan <artem.harov.120@mail.ru>
2022-11-23 19:22:00 +0300

    add views for register & login

--- users/views.py
+++ users/views.py
@@ -3,13 +3,38 @@ from django.urls import reverse
 from django.contrib import auth, messages
 from django.contrib.auth.decorators import login_required
 
+from .forms import UserLoginForm, UserRegistrationForm
+
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user and user.is_active:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
+    return render(request, 'login.html', {
+        'form': form
+    })
 
 
 def register(request):
-    return render(request, 'register.html')
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'You have successfully registered')
+            return HttpResponseRedirect(reverse('login'))
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'register.html', {
+        'form': form
+    })
 
 
 def logout(request):
