6ead48a90fec3aea8744fbe4954fcd26cab73ba2
Fantacy-Fan <artem.harov.120@mail.ru>
2022-11-23 18:57:17 +0300

    edit user static

--- PicturesSite/settings.py
+++ PicturesSite/settings.py
@@ -122,6 +122,7 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'MiracleArt/static'),
+    os.path.join(BASE_DIR, 'users/static'),
 )
 
 # Default primary key field type
