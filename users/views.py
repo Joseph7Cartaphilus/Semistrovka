19c1dfbca7b88a289e52ad39a15540081200189b
Fantacy-Fan <artem.harov.120@mail.ru>
2022-11-23 18:58:09 +0300

    add paths and views for users templates

--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib import auth, messages
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+def login(request):
+    return render(request, 'users/login.html')
+
+
+def register(request):
+    return render(request, 'users/register.html')
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
