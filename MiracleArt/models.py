50d9b87241a0eb17dcf3e5cadd34e40cfbcb4cd5
Fantacy-Fan <artem.harov.120@mail.ru>
2022-12-03 21:44:24 +0300

    add verbose name for all attributes

--- MiracleArt/models.py
+++ MiracleArt/models.py
@@ -12,23 +12,28 @@ class BaseModel(models.Model):
 
 
 class PinCategory(BaseModel):
-    name = models.CharField(max_length=64, unique=True)
-    description = models.TextField(null=True, blank=True)
+    name = models.CharField(max_length=64, unique=True, verbose_name='Название')
+    description = models.TextField(null=True, blank=True, verbose_name='Описание')
 
     class Meta:
-        verbose_name_plural = 'Pin Categories'
+        verbose_name_plural = 'Категории Пинов'
+        verbose_name = 'Категорию Пина'
 
     def __str__(self):
         return self.name
 
 
 class Pin(BaseModel):
-    title = models.CharField(max_length=30, null=False, blank=False)
-    text = models.TextField(max_length=90, null=True, blank=True)
-    img = models.ImageField(upload_to='images/', null=False, blank=False)
-    slug = models.SlugField(default='', db_index=True)
-    category = models.ForeignKey(PinCategory, on_delete=models.PROTECT, blank=True, null=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=30, null=False, blank=False, verbose_name='Название')
+    text = models.TextField(max_length=90, null=True, blank=True, verbose_name='Описание')
+    img = models.ImageField(upload_to='images/', null=False, blank=False, verbose_name='Пин/Картинка')
+    slug = models.SlugField(default='', db_index=True, verbose_name='слаг')
+    category = models.ForeignKey(PinCategory, on_delete=models.PROTECT, blank=True, null=True, verbose_name='Категория')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь')
+
+    class Meta:
+        verbose_name_plural = 'Пины'
+        verbose_name = 'Пин'
 
     def __str__(self):
         return f'Pin {self.id} - {self.title}'
